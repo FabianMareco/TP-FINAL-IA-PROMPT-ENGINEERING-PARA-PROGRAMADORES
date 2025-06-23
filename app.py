31420d4fea8aabb5e3a24cd9cd7bfe796fa6957c
Fabian Mareco <fabian.mareco@gmail.com>
2025-06-23 00:43:38 -0300

    versión final sin problemas de api-key compatible con streamlit

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# --- Tu aplicación normal ---
 st.title("Happblemos - Tu espacio de escucha")
 
-# Resto de tu interfaz...
-user_input = st.text_area("Tu mensaje", height=150)
+user_input = st.text_area("Escribe lo que quieras compartir acerca de como te sentís o como estuvo tu día:", height=150)
 
 if st.button("Happblemos"):
     if user_input:
@@ -21,3 +19,10 @@ if st.button("Happblemos"):
             st.error(f"Ocurrió un error: {str(e)}")
     else:
         st.warning("Por favor, escribí un mensaje antes de enviar.")
+with st.expander("CÓMO FUNCIONA HAPPBLEMOS", expanded=False):
+    st.markdown("""
+    <div class='info-box'>
+        <p>Happblemos es un espacio de escucha, co-creado con lo mejor de la IA, interactuarás de manera segura y podrás poner en palabras lo que te sucede, para luego recibir una devolución con la intención de ayudarte.</p>
+        <p>Puedes escribir lo que te sucede, lo que sea, expresado de la manera que quieras, HAPPBLEMOS está para escucharte y brindarte su contención.</p>
+    </div>
+    """, unsafe_allow_html=True)
